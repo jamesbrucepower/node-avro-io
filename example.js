9ed1be4e1368f4caa548269fcce52dd9cece003c
James Bruce Power <james.bruce.power@gmail.com>
2012-10-24 17:38:59 +0100

    Made a lot of progress with the data reader, now reading at least the first buffer

--- example.js
+++ example.js
@@ -6,6 +6,8 @@ dataFile.open(fileName);
 dataFile.read(function(err, data) {
     if (err) 
         console.error(err);
-    else
-        console.log(JSON.stringify(data, 0, 4));
+    else {
+        if (data.request.path === "/authZ/authorize" && data.response.status == 200)
+            console.log("%s %s %s %s", data.customer.data.username, data.request.body.productUrl, data.time, data.request.headers.referer);
+    }
 });
