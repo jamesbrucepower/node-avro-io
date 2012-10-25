a39d83920bee876406125d0cc95e4c509901f9c7
James Bruce Power <james.bruce.power@gmail.com>
2012-10-25 17:00:18 +0100

    Finally got streaming reading to work, WOOHOOO!

--- example.js
+++ example.js
@@ -8,7 +8,10 @@ dataFile.read(function(err, data) {
     if (err) 
         console.error(err);
     else {
-        if (data.request.path === "/authZ/authorize" && data.response.status == 200)
-            console.log(sprint("%30s: %s", data.customer.data.username, data.request.body.productUrl));
+        if (data.elapsedTime > 5E8)
+            console.log("%dms: %s", Math.floor(data.elapsedTime/1E6), data.request.path);
+        //
+        //if (data.request.path === "/authZ/authorize" && data.response.status == 200)
+          //  console.log(sprint("%30s: %s", data.customer.data.username, data.request.headers["user-agent"]));
     }
 });
