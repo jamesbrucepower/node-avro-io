1cca436f4b811f1abd71813f64f517dde65f92f9
James Bruce Power <james.bruce.power@gmail.com>
2012-10-26 17:13:55 +0100

    Fixed example

--- example.js
+++ example.js
@@ -4,14 +4,14 @@ var DataFile = require(__dirname + "/lib/datafile");
 var fileName = process.argv[2];
 dataFile = DataFile();
 dataFile.open(fileName);
-dataFile.Reader.read(function(err, data) {
+dataFile.Reader.read(null, function(err, data) {
     if (err) 
         console.error(err);
     else {
-        if (data.elapsedTime > 5E8)
-            console.log("%dms: %s", Math.floor(data.elapsedTime/1E6), data.request.path);
+        //if (data.elapsedTime > 5E8)
+          //  console.log("%dms: %s", Math.floor(data.elapsedTime/1E6), data.request.path);
         //
         //if (data.request.path === "/authZ/authorize" && data.response.status == 200)
-          //  console.log(sprint("%30s: %s", data.customer.data.username, data.request.headers["user-agent"]));
+            console.log("%s %s", data.customer.data.username, data.request.body.productUrl);
     }
 });
