a5ad440ee86b36e04dbdaf77ec2ffe9206c4e12e
James Bruce Power <james.bruce.power@gmail.com>
2012-12-10 23:34:43 +0000

    Trying to fix streams

--- example.js
+++ example.js
@@ -4,10 +4,21 @@ var DataFile = require('./index.js').DataFile;
 var avro = DataFile.AvroFile();
 var fileStream = fs.createWriteStream('test.avro');
 
-var schema = 'string';
-var writer = avro.open("test.avro", schema, { flags: 'w', codec: 'deflate' });
+var schema = { 
+    type: "string" 
+};
+var writer = avro.open("test.avro", schema, { flags: 'w', codec: 'null' });
 writer.pipe(fileStream);
 writer
+    .on('data', function(data) {
+        console.log(data);
+    })
+    .on('end', function() {
+        console.log("end()");
+    })
+    .on('close', function() {
+        console.log("close()");
+    })
     .append("The quick brown fox jumped over the lazy dogs")
     .append("Another entry")
-    .end();
+    .end("last one");
